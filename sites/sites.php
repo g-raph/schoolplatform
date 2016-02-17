7f970b2a3c6890914e847daaff1a556080e30ba7
Frederik Wouters <woutersf@gmail.com>
2016-02-17 17:27:54 +0100

    allow per site domain setting

--- sites/sites.php
+++ sites/sites.php
@@ -1,10 +1,24 @@
 <?php
 
 $sites['stmartinus.dev'] = 'stmartinus';
-$sites['sub.schoolplatform.dev'] = 'stmartinus';
 $sites['schoolplatform.dev'] = 'schoolplatform';
 
 /**
  * Write logic here
  */
+
 //Sites folders are dynamic.
+//Allows them to add Own domain.
+
+$sitesList = scandir ( "sites" );
+$ignored = array('.','..','all','default','template');
+$cwd = getcwd();
+foreach($sitesList as $site) {
+  if (!in_array($site, $ignored)) {
+    if (file_exists($cwd . '/sites/' .$site . '/sites.php')) {
+      include_once($cwd . '/sites/' . $site . '/sites.php');
+    }
+  }
+}
+
+
